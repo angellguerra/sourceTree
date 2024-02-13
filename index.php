427b987a72960c848c225fb4a270c942bca2ac3d
Angel Guerra Martinez <angel.guerra.martinez@ces-vegamedia.es>
2024-02-13 11:25:51 +0100

    2º Cambio

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PEDRO SANCHEZ ROJO</title>
+    <title>PEDRO SANCHEZ ROJO ODIO A CHIQUI IBAI</title>
 </head>
 <body>
     
